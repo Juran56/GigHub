3b760450dca10b95b94b1c1d4b049547720d0c05
Pranab Chakma <juran56@gmail.com>
2020-08-11 18:46:50 +0600

    Implement Validations

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -39,10 +39,15 @@ namespace GigHub.Controllers
         {
             //var artist = _context.Users.Single(u => u.Id == artistId);
             //var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            if (!ModelState.IsValid )
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            } 
             var gig = new Gig()
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
